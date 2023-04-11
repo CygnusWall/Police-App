ff92606c186bd40bb418a7c932da4301eb3ed3d2
CygnusWall <96161984+CygnusWall@users.noreply.github.com>
2023-04-11 16:03:53 -0400

    images added to buttons

--- Police-App/Form1.Designer.cs
+++ Police-App/Form1.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button3);
@@ -51,51 +51,61 @@
             this.panel1.Size = new System.Drawing.Size(70, 321);
             this.panel1.TabIndex = 0;
             // 
-            // button1
+            // button5
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(70, 46);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(0, 67);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 46);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(0, 136);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(70, 46);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button5.BackgroundImage = global::Police_App.Properties.Resources.Info_Img;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.Location = new System.Drawing.Point(0, 273);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 48);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(0, 207);
+            this.button4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button4.BackgroundImage = global::Police_App.Properties.Resources.Help_Img;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Location = new System.Drawing.Point(0, 202);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(70, 46);
+            this.button4.Size = new System.Drawing.Size(70, 48);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // button3
             // 
-            this.button5.Location = new System.Drawing.Point(0, 275);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(70, 46);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button3.BackgroundImage = global::Police_App.Properties.Resources.Upload_Img;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Location = new System.Drawing.Point(0, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 48);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.BackgroundImage = global::Police_App.Properties.Resources.Report_Img;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(0, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 48);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.BackgroundImage = global::Police_App.Properties.Resources.Home_Img;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 48);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -113,10 +123,10 @@
         #endregion
 
         private Panel panel1;
+        private Button button1;
         private Button button5;
         private Button button4;
         private Button button3;
         private Button button2;
-        private Button button1;
     }
 }
